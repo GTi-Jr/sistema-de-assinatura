42869f61d9d1f609bf111300ccf0fa9aa88586f0
Caio Ergos <caioergos@gmail.com>
2016-09-15 10:42:21 -0300

    landing page do template + form de login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,40 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//
+//= require ultima/libs/jquery-1.11.2.min
+//= require ultima/libs/jquery.easing.1.3
+//= require ultima/libs/modernizr.custom
+//= require ultima/plugins/bootstrap.min
+//= require ultima/plugins/jquery.touchSwipe.min
+//= require ultima/plugins/jquery.placeholder
+//= require ultima/plugins/icheck.min
+//= require ultima/plugins/jquery.validate.min
+//= require ultima/plugins/gallery
+//= require ultima/plugins/jquery.fitvids
+//= require ultima/plugins/jquery.bxslider.min
+//= require ultima/plugins/jquery.fitvids
+//= require ultima/plugins/chart.min
+//= require ultima/plugins/waypoints.min
+//= require ultima/plugins/smoothscroll
+//= require ultima/landing2
+//
 //= require_tree .
+//
+//<!--Javascript (jQuery) Libraries and Plugins-->
+//<script src="js/libs/jquery-1.11.2.min.js"></script>
+//<script src="js/libs/jquery.easing.1.3.js"></script>
+//<script src="js/plugins/bootstrap.min.js"></script>
+//<script src="js/plugins/jquery.touchSwipe.min.js"></script>
+//<script src="js/plugins/jquery.placeholder.js"></script>
+//<script src="js/plugins/icheck.min.js"></script>
+//<script src="js/plugins/jquery.validate.min.js"></script>
+//<script src="js/plugins/gallery.js"></script>
+//<script src="js/plugins/jquery.fitvids.js"></script>
+//<script src="js/plugins/jquery.bxslider.min.js"></script>
+//<script src="js/plugins/chart.js"></script>
+//<script src="js/plugins/waypoints.min.js"></script>
+//<script src="js/plugins/smoothscroll.js"></script>
+//<script src="js/plugins/color-switcher.js"></script>
+//<script src="mailer/mailer.js"></script>
+//<script src="js/landing2.js"></script>
