81e32a7c5de0fa6ee6868a0068ea7f0593ee7aee
Caio Ergos <caioergos@gmail.com>
2016-09-15 23:22:37 -0300

    login form OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registration',
+    confirmations: 'users/confirmations',
+    unlocks: 'users/unlocks'
+  }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
