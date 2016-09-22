d463df75ff6442bd91185cc830ae00b390d24d88
Caio Ergos <caioergos@gmail.com>
2016-09-22 11:56:54 -0300

    devise controller sanitizer for admins

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -16,10 +16,10 @@ class Admins::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+   protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+   #If you have extra params to permit, append them to the sanitizer.
+   def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+   end
 end
