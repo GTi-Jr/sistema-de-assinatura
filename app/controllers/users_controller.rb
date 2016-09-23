cb482f5c9faf43df1087ff7ac1471d21ec412197
Caio Ergos <caioergos@gmail.com>
2016-09-23 08:14:44 -0300

    parte do perfil pronto

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,63 @@ class UsersController < ApplicationController
   before_action :check_user_completed
 
   def profile
-    @babies = current_user.babies # Carregar os bebês numa variável para nao acessar BD na view
-    current_user.babies.build # Para caso o usuário deseje adicionar um novo bebê
+    build_user_address
+    build_user_baby
+  end
 
-    @addresses = current_user.addresses
-    current_user.addresses.build
+  def update
+    if current_user.update(user_params)
+      redirect_to :back, notice: 'Informações atualizadas'
+    else
+      redirect_to :back, alert: 'Não foi possível atualizar seus dados'
+    end
+  end
+
+  def add_address
+    @address = current_user.addresses.build(address_params)
+
+    if @address.save
+      redirect_to :back, notice: 'Endereço cadastrado'
+    else
+      profile
+      render :profile
+    end
+  end
+
+  def update_addresses
+    if current_user.update(user_addresses_params)
+      redirect_to :back, notice: 'Endereços atualizados'
+    else
+      render :profile
+    end
   end
 
   private
+    def build_user_address
+      @address = Address.new
+    end
+
+    def build_user_baby
+      @baby = Baby.new
+    end
+
     def user_params
-      params.require(:user).permit(:email,:name,:sex,:birthdate,:cpf,:rg,:password, :password_confirmation,baby_attributes: %i(id name born user_id))
+      params.require(:user).permit(:email, :name, :sex, :birthdate, :cpf, :rg, :password, :password_confirmation)
+    end
+
+    def user_addresses_params
+      params.require(:user).permit(address_attributes: %i(street state city zipcode number))
+    end
+
+    def address_params
+      params.require(:address).permit(:street, :state, :city, :zipcode, :number)
+    end
+
+    def user_babies_params
+      params.require(:user).permit(baby_attributes: %i(id name born birthdate weeks user_id))
+    end
+
+    def baby_params
+      params.require(:baby).permit(:name, :born, :birthdate, :weeks)
     end
 end
