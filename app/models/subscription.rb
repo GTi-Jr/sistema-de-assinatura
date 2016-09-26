7fa4fb2cffd42718ac5cc537f14e13a23d565b55
Caio Ergos <caioergos@gmail.com>
2016-09-26 20:38:44 -0300

    fix active? logic in subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,6 +4,6 @@ class Subscription < ActiveRecord::Base
   has_one :plan_user, class_name: 'Plan', foreign_key: 'subscription_id'
 
   def active?
-    !canceled_on.nil?
+    canceled_on.nil?
   end
 end
