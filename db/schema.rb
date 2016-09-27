f9303e15a38f920560b00ebd049a04a0e6df9c7f
Caio Ergos <caioergos@gmail.com>
2016-09-27 00:50:28 -0300

    perfil quase terminado #TODO adicionar link p/ escolher planos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926005230) do
+ActiveRecord::Schema.define(version: 20160926003756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,10 +61,10 @@ ActiveRecord::Schema.define(version: 20160926005230) do
     t.string   "name"
     t.string   "duration"
     t.string   "description"
+    t.integer  "subscription_id"
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
     t.decimal  "price",           precision: 8, scale: 2
-    t.integer  "subscription_id"
   end
 
   create_table "subscriptions", force: :cascade do |t|
