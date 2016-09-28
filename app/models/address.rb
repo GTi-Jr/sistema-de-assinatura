f4e0868e19e3154c3b7a2c09c9279b2cced4a80d
Caio Ergos <caioergos@gmail.com>
2016-09-28 11:22:12 -0300

    fix address before_destroy

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to :user
 
+  before_destroy :update_main_address_after_destroy
+
   validates_presence_of :street
   validates_presence_of :zipcode
   validates_presence_of :number
@@ -15,4 +17,14 @@ class Address < ActiveRecord::Base
     Address.where(user: user).where.not(id: id).update_all(main: false)
     update(main: true)
   end
+
+  private
+    def update_main_address_after_destroy
+      other_user_addresses = Address.where(user_id: user_id).where.not(id: id)
+      if other_user_addresses.count > 0
+        other_user_addresses.first.update(main: true)
+      else
+        false
+      end
+    end
 end
