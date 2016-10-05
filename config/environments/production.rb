d234ce3f814703e854d1d19705e8204fe84e60ed
mmendesc <matheus.mendescf@gmail.com>
2016-10-04 23:01:19 -0300

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
-
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
