a216f8ca15ed00c334138b9af753cbf1bb431e47
mmendesc <matheus.mendescf@gmail.com>
2016-10-06 00:46:21 -0300

    User not registrable

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :registerable,
+         #:registerable,
          :recoverable,
          :rememberable,
          :trackable,
@@ -65,7 +65,7 @@ class User < ActiveRecord::Base
   end
 
   protected
- 
+
     def subscribe_user_to_mailing_list
       SubscribeUserToMailingListJob.perform_later(self)
     end
