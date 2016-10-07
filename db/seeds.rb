e6c9137b3584e5818530467945a480cdfd320dc3
Caio Ergos <caioergos@gmail.com>
2016-10-07 01:59:08 -0300

    seed plans

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+plan_1 = Plan.new name: 'Plano Mensal', duration: 1, price: 74.90, description: 'Todo mês um mundo de surpresas chegando até você!'
+plan_2 = Plan.new name: 'Plano Trimestral', duration: 3, price: 209.70, description: 'Três meses de muito carinho e muitas novidades!'
+plan_3 = Plan.new name: 'Plano Semestral', duration: 6, price: 389.40, description: 'Durante seis meses você recebe uma linda caixinha preparada especialmente para você!'
+
+plan_1.save
+plan_2.save
+plan_3.save
