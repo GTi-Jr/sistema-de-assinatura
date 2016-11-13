0fe8b8f56d1a5484146898847960049c1a92afd4
Caio Ergos <caioergos@gmail.com>
2016-11-13 11:15:17 -0300

    implementação webhooks incompleta

--- app/controllers/iugu/webhooks_controller.rb
+++ app/controllers/iugu/webhooks_controller.rb
@@ -3,10 +3,12 @@ class Iugu::WebhooksController < ApplicationController
 
   def webhook
     case params[:event]
-    when 'subscription.created'
+    when 'subscription.created'   then subscription_created
     when 'subscription.activated' then subscription_activated
-    when 'subscription.suspended'
-    when 'subscription.expired'
+    when 'subscription.suspended' then subscription_suspended
+    when 'subscription.expired'   then subscription_expired
+    when 'invoice.created'        then invoice_created
+    when 'invoice.status_changed' then invoice_status_changed
     end
 
     render nothing: true
@@ -14,9 +16,31 @@ class Iugu::WebhooksController < ApplicationController
 
   private
 
-  def subscription_activated
+  def subscription_created
     iugu_subscription = Iugu::Subscription.fetch(params[:data][:subscription_id])
+    last_invoice = iugu_subscription.recent_invoices[0]
+
     @user = User.find_by(customer_id: iugu_subscription.customer_id)
-    @subscription = @user.subscriptions.create(plan: @plan, iugu_id: iugu_subscription.id)
+    @subscription = @user.subscriptions.create(
+      plan: @plan,
+      iugu_id: iugu_subscription.id,
+      iugu_payment_status:  last_invoice['status']
+    )
+  end
+
+  def subscription_activated
+  end
+
+  def subscription_suspended
+  end
+
+  def subscription_expired
+  end
+
+  def invoice_created
+  end
+
+  def invoice_status_changed
   end
 end
+
