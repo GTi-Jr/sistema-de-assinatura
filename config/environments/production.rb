e6d5411342f09efab3d42e7a22559d17fe1af64f
Caio Ergos <caioergos@gmail.com>
2016-12-03 19:25:44 -0300

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options[:host] = 'www.caixadacegonha.com.br'
 
   ActionMailer::Base.smtp_settings = {
-    :addres           => 'smtp.sendgrid.net',
+    :address          => 'smtp.sendgrid.net',
     :port             => 587,
     :authentication   => :plain,
     :user_name        => ENV['SENDGRID_USERNAME'],
