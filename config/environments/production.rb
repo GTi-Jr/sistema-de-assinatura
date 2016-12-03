377f64c95c71bbef2fbec54ffb195c36472c43b8
Caio Ergos <caioergos@gmail.com>
2016-12-02 21:58:01 -0300

    sendgrid fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,6 @@ Rails.application.configure do
     :authentication   => :plain,
     :user_name        => ENV['SENDGRID_USERNAME'],
     :password         => ENV['SENDGRID_PASSWORD'],
-    :domain           => ENV['SENDGRID_DOMAIN']
+    :domain           => 'www.caixadegonha.com.br'
   }
-
-
 end
